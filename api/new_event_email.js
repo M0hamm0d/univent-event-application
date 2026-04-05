25b33722fe97dd050c676cb5aa2265d5268bf893
Muhammad Abdussalam <masalami042@gmail.com>
2026-04-05 09:23:27 +0100

    fixed new event email bug

--- api/new_event_email.js
+++ api/new_event_email.js
@@ -12,7 +12,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASSWORD, // Must be an App Password
+    pass: process.env.EMAIL_PASS,
   },
 })
 
